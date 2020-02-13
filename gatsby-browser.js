bbe36ad7c4ec4650a244550e68a5a95382d61bbd
iraguhalionel <liraguha00@gmail.com>
2020-02-13 16:17:24 +0200

    Deleting Redudant files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
-import "@babel/polyfill"
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
 
-// eslint-disable-next-line import/prefer-default-export
-export const onClientEntry = () => {
-  // Without this function body the import will not be picked up.
-}
+// You can delete this file if you're not using it
