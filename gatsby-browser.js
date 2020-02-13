59a598ee0286b14e60862add4c5ad403d90b82c7
iraguhalionel <liraguha00@gmail.com>
2020-02-13 13:16:25 +0200

    Updating npm

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,10 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import { globalHistory } from "@reach/router"
 
-// You can delete this file if you're not using it
+export const onInitialClientRender = () => {
+  /**
+   * This is a workaround for a bug in Gatsby
+   *
+   * See https://github.com/gatsbyjs/gatsby/issues/8357 for more details
+   */
+  globalHistory._onTransitionComplete()
+}
