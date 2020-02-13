94361c7236ef89954921e2ae9c17bb57f16abebe
iraguhalionel <liraguha00@gmail.com>
2020-02-13 13:34:25 +0200

    Adding the content to ss

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,10 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import { globalHistory } from "@reach/router"
 
-// You can delete this file if you're not using it
+export const onInitialClientRender = () => {
+  /**
+   * This is a workaround for a bug in Gatsby
+   *
+   * See https://github.com/gatsbyjs/gatsby/issues/8357 for more details
+   */
+  globalHistory._onTransitionComplete()
+}
